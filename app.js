0b704934ec58f7c85eebaf651a3e6db46687d2ec
SupaPist <brodywilliams422@gmail.com>
2018-09-25 23:55:12 -0500

    updated functions

--- app.js
+++ app.js
@@ -1,53 +1,125 @@
-const render = function() {
-    $('.content').empty();
-  
-    for( let i = 0; i < employeeList.length; i++ ) {
-      $('.content').append(`<p>${employeeList[i].name}</p><p>${employeeList[i].officeNum}</p><p>${employeeList[i].phoneNum}</p>`);
-    }
-  }
-  
-  render();
 
 
 
-const getInputVal = function() {
-   const nameVal = $('#name').val();
-    employeeList.push(nameVal);
-  
-    $('#name').val('');
-    render();
-  }
-  
-  $('#add').on('click', getInputVal);
 
+// displays contact information
 
+const view = function (content) {
+  $('.content').empty();
 
-  
-
-const blueBody = function() {
-    const nameVal = $('#name').val();
-  
-    if(employeeList.includes(nameVal)) {
-      $('body').addClass('blue');
-    }
-  
-
-    $('#name').val('');
-  }
-  
-  $('#search').on('click', blueBody);
-
-
-
-const removeName = function() {
-   const nameVal = $('#name').val();
-  
-  
-   employeeList.splice(employeeList.indexOf(nameVal), 1);
-  
-
- $('#name').val('');
-    render();
+  for (let i = 0; i < employeeList.length; i++) {
+    $('.content').append(`<div class='card'><p>${employeeList[i].name}</p><p>${employeeList[i].officeNum}</p><p>${employeeList[i].phoneNum}</p></div>`);
+  };
 }
-  
-$('#delete').on('click', removeName);
+$("#view").on("click", view);
+
+
+// adds contact information
+
+$("#add").on("click", function (event) {
+  $('.content').empty();
+  $('.verifyForm').addClass("hide");
+  $('.addForm').removeClass("hide");
+})
+
+$("#addEmployee").on("click", function (event) {
+  event.preventDefault();
+  const name = $("#name").val();
+  const officeNum = $("#officeNum").val();
+  const phoneNum = $("#phoneNum").val();
+  let newEmployee = {
+    name: name,
+    officeNum: officeNum,
+    phoneNum: phoneNum
+  }
+  if (name === "" || officeNum === "" || phoneNum === "") {
+    alert("Didnt Fill Out")
+  } else {
+    employeeList.push({
+      name: name,
+      officeNum: officeNum,
+      phoneNum: phoneNum})
+      render();
+    }
+  }
+)
+
+
+$("#add").on("click", view);
+
+//verifys if the employee is in the contact book
+
+$("#verify").on("click", function (event) {
+  $('.content').empty();
+  $('.addForm').addClass("hide");
+  $('.verifyForm').removeClass("hide");
+})
+
+$("#verifyEmployee").on("click", function (event) {
+  event.preventDefault();
+  const name = $("#name").val();
+  if (name === $("#name").val()){
+    alert("Currently an Employee")
+  } else {
+   alert("Not an Employee")
+      render();
+    }
+  }
+)
+
+$("#verify").on("click", view);
+
+
+
+
+// $("#update").on("click", function (event) {
+//   $('.content').empty();
+//   $('.verifyForm').addClass("hide");
+//   $('.addForm').removeClass("hide");
+// })
+
+// $("#updateEmployee").on("click", function (event) {
+//   event.preventDefault();
+//   const name = $("#name").val();
+//   const officeNum = $("#officeNum").val();
+//   const phoneNum = $("#phoneNum").val();
+//   let newEmployee = {
+//     name: name,
+//     officeNum: officeNum,
+//     phoneNum: phoneNum
+//   }
+
+//   const age = prompt('Enter Employee Name');
+
+// function updateEmployee(event) {
+//   if (name === "#name" ) {
+//   } else {
+//     alert('You did not input your age');
+//   }
+// }
+
+// checkEmployee();
+
+
+//   if (name === "" || officeNum === "" || phoneNum === "") {
+//     alert("Didnt Fill Out")
+//   } else {
+//     employeeList.push({
+//       name: name,
+//       officeNum: officeNum,
+//       phoneNum: phoneNum})
+//       render();
+//     }
+//   }
+// )
+
+
+
+const render = function() {
+  $('.content').empty();
+  let employees = "";
+
+  for (let i = 0; i < employeeList.length; i++) {
+    $('.content').append(`<div class='card'><p>${employeeList[i].name}</p><p>${employeeList[i].officeNum}</p><p>${employeeList[i].phoneNum}</p></div>`);
+  };
+}
